ef239b468ec50ceadba231da8e262c274001e900
SergioYaguchi <seryagua.2004@gmail.com>
2024-04-19 17:04:28 +0200

    Avancement S3

---
+++ projet133/Rest2/rest2/src/main/java/com/example/rest2/controller/Controller.java
@@ -0,0 +1,5 @@
+package com.example.rest2.controller;
+
+public class Controller {
+    
+}
---
+++ projet133/Rest2/rest2/src/main/java/com/example/rest2/model/Score.java
@@ -0,0 +1,54 @@
+package com.example.rest2.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_score")
+
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PK_Score")
+    private Integer id;
+    
+    @Column(name = "Point")
+    private Integer point;
+
+    @Column(name = "Date", length = 50)
+    private String date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_user")
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+
+    
+
+    
+
+    
+
+    
+
+    
+}
