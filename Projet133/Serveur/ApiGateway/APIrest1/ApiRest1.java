0a5ce0e6f90067b49bfe49c2053decdc2a3295c4
SergioYaguchi <seryagua.2004@gmail.com>
2024-04-19 14:11:20 +0200

    Création de l'aborecences

---
+++ Projet133/Serveur/ApiGateway/APIrest1/ApiRest1.java
@@ -0,0 +1,5 @@
+package Projet133.Serveur.ApiGateway.APIrest1;
+
+public class ApiRest1 {
+    
+}
---
+++ Projet133/Serveur/ApiGateway/APIrest2/ApiRest2.java
@@ -0,0 +1,5 @@
+package Projet133.Serveur.ApiGateway.APIrest2;
+
+public class ApiRest2 {
+    
+}
