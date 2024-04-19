87ac16057d8fc867d3a68d2ab6f823a27d13980d
SergioYaguchi <seryagua.2004@gmail.com>
2024-04-19 17:04:08 +0200

    Avancement S3

---
+++ projet133/Rest2/rest2/src/main/java/com/example/rest2/model/User.java
@@ -0,0 +1,43 @@
+package com.example.rest2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PK_User")
+    private Integer id;
+
+    @Column(name = "Username", length = 50)
+    private String username;
+
+    @Column(name = "Password", length = 50)
+    private String password;
+
+    public Integer getId(){
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String nom) {
+        this.username = nom;
+    }
+}
+
+    
+
+    
+    
---
+++ projet133/Rest2/rest2/src/main/java/com/example/rest2/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.rest2.service;
+
+public class UserService {
+    
+}
