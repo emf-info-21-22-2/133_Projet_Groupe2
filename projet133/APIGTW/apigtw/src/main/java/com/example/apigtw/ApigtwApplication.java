ea2f3a739f94116c80cc69d277af9b2ffdd1add6
NaelCurrat <nael.currat@gmail.com>
2024-04-26 13:55:28 +0200

    Creation APIGTW

---
+++ projet133/APIGTW/apigtw/src/main/java/com/example/apigtw/ApigtwApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigtwApplication.class, args);
+	}
+
+}
---
+++ projet133/APIGTW/apigtw/src/test/java/com/example/apigtw/ApigtwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigtw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
