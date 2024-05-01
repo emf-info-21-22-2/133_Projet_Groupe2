3897589d8604b56056967c9ff7dc4f57dd76d5fb
SergioYaguchi <seryagua.2004@gmail.com>
2024-05-01 12:48:48 +0200

    Codage du controller

--- projet133/APIGTW/apigtw/src/main/java/com/example/apigtw/Controller/Controller.java
+++ projet133/APIGTW/apigtw/src/main/java/com/example/apigtw/Controller/Controller.java
@@ -1,12 +1,24 @@
 package com.example.apigtw.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.apigtw.Service.Rest1Service;
+import com.example.apigtw.Service.Rest2Service;
+
 import jakarta.servlet.http.HttpSession;
 
 public class Controller {
+    private final Rest1Service rest1;
+    private final Rest2Service rest2;
+
+    @Autowired
+    public Controller(Rest1Service rest1, Rest2Service rest2) {
+        this.rest1 = rest1;
+        this.rest2 = rest2;
+    }
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpSession session) {
--- projet133/APIGTW/apigtw/src/main/java/com/example/apigtw/Service/Rest2Service.java
+++ projet133/APIGTW/apigtw/src/main/java/com/example/apigtw/Service/Rest2Service.java
@@ -69,5 +69,47 @@ public class Rest2Service {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Erreur lors de la création de l'utilisateur");
     }
-    
+
+    public ResponseEntity<String> deleteUser(String username) {
+        // Appeler votre API Gateway pour gérer la déconnexion
+        String url = baseUrl + "/deleteUser";
+
+        // Effectuer la requête POST pour la déconnexion
+        ResponseEntity<String> response = restTemplate.postForEntity(url, username, String.class);
+
+        // Retourner la réponse avec le code de statut approprié et un message
+        if (response.getStatusCode().is2xxSuccessful()) {
+            // Succès (code de statut dans la plage des 2xx)
+            return ResponseEntity.ok("User supprimée");
+        } else {
+            // Erreur (code de statut dans la plage des 4xx)
+            return ResponseEntity.badRequest().body("Error durant la suppression");
+        }
+    }
+    public ResponseEntity<String> addScoreUser(Integer point, Integer fk_user) {
+        String url = baseUrl + "/addScoreUser"; // Ajustez selon l'API que vous appelez
+
+        // Préparer la requête
+        Map<String, Object> credentials = new HashMap<>();
+        credentials.put("point", point);
+        credentials.put("fk_user", fk_user);
+
+        // Effectuer l'appel API et recevoir la réponse
+        ResponseEntity<String> response = restTemplate.postForEntity(url, credentials, String.class);
+
+        // Supposons que l'API renvoie un statut 200 avec un corps contenant
+        if (response.getStatusCode().is2xxSuccessful()) {
+
+            // Configurer la session en cas de succès
+
+            return ResponseEntity.ok("Score ajouté");
+
+        }
+
+        // Si quelque chose ne va pas, renvoyer une erreur
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erreur lors de l'ajout");
+    }
+
+
 }
