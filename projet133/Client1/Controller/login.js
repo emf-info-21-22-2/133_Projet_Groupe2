cb88fd10e98e7ea594ddd2e905b869d3d354e0c3
NaelCurrat <nael.currat@gmail.com>
2024-06-20 16:11:35 +0200

    Client avancement

--- projet133/Client1/Controller/login.js
+++ projet133/Client1/Controller/login.js
@@ -1,14 +1,33 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const errorMessage = document.getElementById('error-message');
-
-    if (username === "user" && password === "password") {
-        alert("Login réussi!");
-        errorMessage.textContent = "";
-    } else {
-        errorMessage.textContent = "Identifiant ou mot de passe incorrect.";
+class LoginCtrl{
+	
+    constructor() {
+        this.init();
     }
-});
+
+    creerEntraineur() {
+        var username = document.getElementById("TXTusername").value;
+        var password = document.getElementById("TXTpassword").value;
+
+        loginConnecter(username, password, this.loginSuccess.bind(this), this.loginError.bind(this));
+    }
+
+    loginSuccess(data) {
+        alert("User créé avec succès!", data);
+        //window.location.href = "app.html";
+        
+    }
+
+    loginError(data) {
+        alert("User incorrecte ou deja créer !", data);
+    }
+
+
+    init() {
+        var btnLogin = document.getElementById("buttonLogin");
+        btnLogin.addEventListener('click', () => this.creerUser());
+
+    }
+}
+$(document).ready(function () {
+    var loginCTRL = new LoginCtrl();
+  });
