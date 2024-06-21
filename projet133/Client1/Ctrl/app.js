e8253adc5331d047ca9b9237bea1919407ecaf69
NaelCurrat <nael.currat@gmail.com>
2024-06-21 09:07:27 +0200

    Commit

--- projet133/Client1/Ctrl/app.js
+++ projet133/Client1/Ctrl/app.js
@@ -3,19 +3,22 @@ document.addEventListener("DOMContentLoaded", function() {
     chargerQuestions(function(data) {
         console.log("Données reçues : ", data);
         const questionsList = document.getElementById("questionsList");
-        
+
+        // Parcourir les questions et leurs réponses
         for (let questionText in data) {
+            // Créer un élément pour la question
             const questionElement = document.createElement("div");
-            questionElement.classList.add("question");
-            questionElement.textContent = questionText;
+            questionElement.classList.add("enoncer");
+            questionElement.textContent = questionText; // Le texte de la question
             questionsList.appendChild(questionElement);
 
+            // Créer une liste pour les réponses
             const answersList = document.createElement("ul");
             answersList.classList.add("answers");
             data[questionText].forEach(function(answer) {
                 const answerItem = document.createElement("li");
                 answerItem.classList.add("answer");
-                answerItem.textContent = answer;
+                answerItem.textContent = answer; // Le texte de la réponse
                 answersList.appendChild(answerItem);
             });
 
@@ -25,5 +28,5 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("Échec du chargement des questions :", status, error);
         const questionsList = document.getElementById("questionsList");
         questionsList.textContent = "Échec du chargement des questions.";
-    })
+    });
 });
